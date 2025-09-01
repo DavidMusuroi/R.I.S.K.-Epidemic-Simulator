4161367803aaec995a49700ee3e661a29ea6d442
David_M <titans156@gmail.com>
2025-09-01 11:51:02 +0300

    Changed file names

---
+++ core/core.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Region{
+    
+};
+
+class Card{
+    public:
+        string name, suit;
+        int value;
+
+    Card(string name, string suit, int value){
+        this->name = name;
+        this->suit = suit;
+        this->value = value;
+    }
+};
+
+int main(){
+    
+}
