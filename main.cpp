870a24f76c5f84b6a96b5af44ec60a66755359e8
David_M <titans156@gmail.com>
2025-09-01 14:51:24 +0300

    Added the main file for the project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
+#include <iostream>
 #include <string>
 #include <fmt/core.h>
-#include <core/include/country.hpp>
+#include "core/include/country.hpp"
 
 using namespace std;
+using namespace fmt;
 
 int main(){
     string name;
     int population;
-    
+    print("Enter the country name: ");
+    getline(cin, name);
+    print("Enter the number of people living here: ");
+    cin >> population;
+    Country c(name, population);
+    print("The country's name is {}, and has a total of {} people\n", c.get_name(), c.get_s());
 }
