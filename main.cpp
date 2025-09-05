f7f41e5f639525b63c8fad4e84ccc5d0ed84310f
David_M <titans156@gmail.com>
2025-09-05 17:32:12 +0300

    Added the choice to select a region and changed some things about selecting the country

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace fmt;
 
 int main(){
     string name, line, choice, tmp, abbv;
-    int population, nr_countries = 0, i;
+    int country_population, nr_countries = 0, country_index, region_index, nr_modifiers, nr_regions = 0, i;
     Country **countries = new Country*[2];
     bool okay = false;
     ifstream csv_countries("../data/countries.csv");
@@ -19,34 +19,103 @@ int main(){
     // Header line
     getline(csv_countries, line);
     print("The countries you can choose from are:\n");
-    // While we can read from the csv
+
+    // Reading the countries from the CSV
     while(getline(csv_countries, line)){
-        // Create a stringstream object to separate the columns
+        // Created a stringstream object to separate the columns
         stringstream ss(line);
         getline(ss, name, ',');
         getline(ss, tmp, ',');
         getline(ss, abbv);
-        // Transform the population to integer and initialize the countries
-        population = stoi(tmp);
-        countries[nr_countries] = new Country(name, population, abbv);
+        if (!abbv.empty() && abbv.back() == '\r') {
+            abbv.pop_back();
+        }
+        // Transformed the population to integer and initialize the countries
+        country_population = stoi(tmp);
+        countries[nr_countries] = new Country(name, country_population, abbv);
         print("{}\n", countries[nr_countries]->get_name());
         nr_countries++;
     }
+
+    nr_countries--;
+
+    // Choosing your country
     while(okay == false){
         print("Enter which country you would like to play as:\n");
         getline(cin, choice);
-        for(i = 0; i < nr_countries && okay == false; i++){
-            if(countries[i]->get_name() == choice){
+        for(country_index = 0; country_index <= nr_countries && okay == false; country_index++){
+            if(countries[country_index]->get_name() == choice){
                 okay = true;
             }
         }
         if(okay == false){
             print("ERROR! Incorect country! The countries you can choose from are:\n");
-            for(i = 0; i < nr_countries; i++){
+            for(i = 0; i <= nr_countries; i++){
                 print("{}\n", countries[i]->get_name());
             }
         }
     }
-    i--;
-    print("You went for {}, which has the abbreviation: {}\n", countries[i]->get_name(), countries[i]->get_abbreviation());
+
+    country_index--;
+
+    print("You went for {}, which has the abbreviation {} and the index {}\n", countries[country_index]->get_name(), countries[country_index]->get_abbreviation(), country_index);
+
+    if(countries[country_index]->get_name() == "Romania"){
+        int county_population;
+        Region **regions = new Region*[42];
+        string modifier1, modifier2;
+        nr_modifiers = 2;
+        // 2 Header lines
+        getline(csv_RO_counties, line);
+        getline(csv_RO_counties, line);
+        print("The counties you can choose from are:\n");
+
+        // Reading the counties from the CSV
+        while(getline(csv_RO_counties, line)){
+            char **modifiers = new char*[2];
+            // Created a stringstream object to separate the columns
+            stringstream ss(line);
+            getline(ss, name, ',');
+            getline(ss, abbv, ',');
+            getline(ss, tmp, ',');
+            getline(ss, modifier1, ',');
+            getline(ss, modifier2);
+            if (!modifier2.empty() && modifier2.back() == '\r') {
+                modifier2.pop_back();
+            }
+            // Transformed the population to integer, and stored modifiers correctly, as well as initialized the counties
+            county_population = stoi(tmp);
+            modifiers[0] = strdup(modifier1.c_str());
+            modifiers[1] = strdup(modifier2.c_str());
+            regions[nr_regions] = new Region(name, county_population, abbv, modifiers);
+            if(nr_regions != 41){
+                print("{}, ", regions[nr_regions]->get_name());
+                nr_regions++;
+            }
+            else{
+                print("{}\n", regions[nr_regions]->get_name());
+            }
+        }
+
+        okay = false;
+
+        //Choosing your county
+        while(okay == false){
+            print("Enter which county you would to start as:\n");
+            getline(cin, choice);
+            for(region_index = 0; region_index <= nr_regions && okay == false; region_index++){
+                if(regions[region_index]->get_name() == choice){
+                    okay = true;
+                }
+            }
+            if(okay == false){
+                print("ERROR! Incorect county! The counties you can choose from are:\n");
+                for(i = 0; i <= nr_regions; i++){
+                    print("{}\n", regions[i]->get_name());
+                }
+            }
+        }
+    region_index--;
+    print("You went for {}, which has the abbreviation {}, the index {}, and the modifiers: {} and {}\n", regions[region_index]->get_name(), regions[region_index]->get_abbreviation(), region_index, regions[region_index]->get_modifier(0), regions[region_index]->get_modifier(1));
+    }
 }
