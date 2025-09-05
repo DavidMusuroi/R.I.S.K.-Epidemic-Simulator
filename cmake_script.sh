a4fb8e03849ebc736edeca0780c29d461710b79c
David_M <titans156@gmail.com>
2025-09-05 15:45:35 +0300

    Modified the script to not have so many clock skew errors

--- cmake_script.sh
+++ cmake_script.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 set -e
 
-# Remove the old build, add the new build and normalize timestamps
-find . -type f -exec touch {} +
-sleep 1
+# Remove the old build, add the new build
 rm -rf build/
 mkdir build
 cd build/
 
 # Run cmake
-if ! cmake ..; then
+if ! cmake --fresh ..; then
     echo "cmake configuration failed. Cleaning up..."
     cd ..
     rm -rf build/
     exit 1
 fi
 
+# Normalize timestamps
+
+find . -type f -exec touch {} +
+sleep 1
+
 # Run build
-if ! cmake --build .; then
+if ! cmake --build . --clean-first; then
     echo "cmake build failed. Cleaning up..."
     cd ..
     rm -rf build/
