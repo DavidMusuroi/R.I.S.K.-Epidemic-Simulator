2ff82509e544216a0827f8f9b97600c5d34ba46d
David_M <titans156@gmail.com>
2025-09-10 11:30:29 +0300

    Added a check so that the total doesn't go higher than the infected, recovered or killed

--- core/src/country.cpp
+++ core/src/country.cpp
@@ -6,18 +6,27 @@ Country::Country(const string &name, int population, const string &abbreviation)
 
 void Country::recover(int p){
     int total = i * p / 100;
+    if(total > i){
+        total = i;
+    }
     i -= total;
     r += total;
 }
 
 void Country::infect(int p){
     int total = s * p / 100;
+    if(total > s){
+        total = s;
+    }
     s -= total;
     i += total;
 }
 
 void Country::kill(int p){
     int total = i * p / 100;
+    if(total > i){
+        total = i;
+    }
     i -= total;
     k += total;
 }
